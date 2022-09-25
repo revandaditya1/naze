09bf0a33c743fa758d3016dcbe607528f0453066
revandaditya1 <113486346+revandaditya1@users.noreply.github.com>
2022-09-25 21:11:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
+global.owner = ['628587515836']
 global.ownernomer = "6285875158363"
 global.premium = ['6285875158363']
 global.packname = 'Sticker  Naze'
